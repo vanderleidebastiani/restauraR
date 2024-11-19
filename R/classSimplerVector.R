ae79edb6fc757ae33de2cd4a2747698edfedb90f
Vanderlei Debastiani <vanderleidebastiani@yahoo.com.br>
2024-11-19 11:27:41 -0300

    New auxiliary function to shiny.

--- R/classSimplerVector.R
+++ R/classSimplerVector.R
@@ -1,3 +1,11 @@
+#' @title Internal function to return the vector class.
+#' @encoding UTF-8
+#' @param x A vector.
+#' @returns The vector class. 
+#' @author See \code{\link{resbiota-package}}.
+#' @seealso \code{\link{simulateCommunities}}
+#' @keywords Auxiliary
+#' @export
 classSimplerVector <- function(x){
   resClass <- class(x)
   resClass <- ifelse(resClass == "integer", "numeric", resClass)
