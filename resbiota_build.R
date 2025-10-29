31a607c66b2c4715d9b059e4fd1554e57257c391
Andre Coutinho <andreganemcoutinho@gmail.com>
2025-10-29 18:27:42 -0300

    Update DESCRIPTION and resbiota_build files

--- resbiota_build.R
+++ resbiota_build.R
@@ -17,9 +17,9 @@ system("R CMD build .")
 # system("R CMD check resbiota")
 # remove.packages("resbiota")
 
-system("R CMD INSTALL resbiota_0.0.3.tar.gz")
+system("R CMD INSTALL resbiota_0.0.4.tar.gz")
 # system("R CMD check --as-cran resbiota_0.0.2.tar.gz")
-system("R CMD check resbiota_0.0.3.tar.gz")
+system("R CMD check resbiota_0.0.4.tar.gz")
 
 require(resbiota)
 ?resbiota::simulateCommunities
